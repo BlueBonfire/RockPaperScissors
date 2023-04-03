86d2500f919b5060f1b0180c7f70df129fdbec9f
Abbasov Samir <92527373+BlueBonfire@users.noreply.github.com>
2023-04-03 10:18:44 +0400

    buttonlar vaistesi ile oyunu oynamaq mumkun olacaq

--- js/script.js
+++ js/script.js
@@ -28,41 +28,121 @@ function playRound(playerSelection, computerSelection) {
 }
 
 
+// function game() {
+//     var player = 0;
+//     var computer = 0;
+//     for (let i = 0; i < 5; i++) {
+//         if (player >= 3 || computer >= 3) {
+//             break;
+//         } else {
+//             const selectionPrompt = prompt("Rock , Paper or Scissors", '');
+//             const playerSelection = selectionPrompt != null ? selectionPrompt.toLowerCase() : null;
+//             if (playerSelection == null) return;
+//             const computerSelection = getComputerChoice();
+//             console.log(computerSelection);
+//             if (playerSelection == 'rock' || playerSelection == 'paper' || playerSelection == 'scissor') {
+//                 console.log(playRound(playerSelection, computerSelection));
+//                 if (playRound(playerSelection, computerSelection) == "You Win! Rock beats Scissor" || playRound(playerSelection, computerSelection) == "You Win! Scissor beats Paper" || playRound(playerSelection, computerSelection) == "You Win! Paper beats Rock") {
+//                     player++;
+//                 } else if (playRound(playerSelection, computerSelection) == "You Lose! Paper beats Rock" || playRound(playerSelection, computerSelection) == "You Lose! Rock beats Scissor" || playRound(playerSelection, computerSelection) == "You Lose! Scissor beats Paper") {
+//                     computer++;
+//                 } //else {
+//                 //     player++;
+//                 //     computer++;
+//                 // }
+//             } else {
+//                 alert("This is not a valid value");
+//                 i--;
+//             }
+//         }
+//     }
+
+//     if (player > computer) {
+//         alert("You Win!");
+//     }else if (player < computer) {
+//         alert("You Lose!");
+//     } else {
+//         alert("Draw!");
+//     }
+// }
+// game();
+
+let player = 0;
+let computer = 0;
+let playerSelection = '';
+let result = '';
+const mainResult = document.getElementById("result");
+const reset = document.getElementById("reset");
+const display = document.getElementById("root");
+const playerScore = document.getElementById("playerScore");
+const playerChoice = document.getElementById("playerChoice");
+const computerScore = document.getElementById("computerScore");
+const computerChoice = document.getElementById("computerChoice");
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+      playerSelection = button.id;
+      //console.log(playerSelection);
+      game();
+      playerScore.innerText = player;
+      computerScore.innerText = computer;
+  });
+});
+
+
 function game() {
-    var player = 0;
-    var computer = 0;
-    for (let i = 0; i < 5; i++) {
-        if (player >= 3 || computer >= 3) {
-            break;
-        } else {
-            const selectionPrompt = prompt("Rock , Paper or Scissors", '');
-            const playerSelection = selectionPrompt != null ? selectionPrompt.toLowerCase() : null;
-            if (playerSelection == null) return;
-            const computerSelection = getComputerChoice();
-            console.log(computerSelection);
-            if (playerSelection == 'rock' || playerSelection == 'paper' || playerSelection == 'scissor') {
-                console.log(playRound(playerSelection, computerSelection));
-                if (playRound(playerSelection, computerSelection) == "You Win! Rock beats Scissor" || playRound(playerSelection, computerSelection) == "You Win! Scissor beats Paper" || playRound(playerSelection, computerSelection) == "You Win! Paper beats Rock") {
-                    player++;
-                } else if (playRound(playerSelection, computerSelection) == "You Lose! Paper beats Rock" || playRound(playerSelection, computerSelection) == "You Lose! Rock beats Scissor" || playRound(playerSelection, computerSelection) == "You Lose! Scissor beats Paper") {
-                    computer++;
-                } //else {
-                //     player++;
-                //     computer++;
-                // }
-            } else {
-                alert("This is not a valid value");
-                i--;
-            }
-        }
+    
+    if (player >= 5 || computer >= 5) return;
+    let showResult;
+    
+    const computerSelection = getComputerChoice();
+    //console.log(computerSelection);
+    computerChoice.innerText = computerSelection;
+    playerChoice.innerText = playerSelection;
+    if (playerSelection == 'rock' || playerSelection == 'paper' || playerSelection == 'scissor') {
+        showResult = playRound(playerSelection, computerSelection);
+        display.innerText = showResult;
+        if (playRound(playerSelection, computerSelection) == "You Win! Rock beats Scissor" || playRound(playerSelection, computerSelection) == "You Win! Scissor beats Paper" || playRound(playerSelection, computerSelection) == "You Win! Paper beats Rock") {
+            player++;
+            
+        } else if (playRound(playerSelection, computerSelection) == "You Lose! Paper beats Rock" || playRound(playerSelection, computerSelection) == "You Lose! Rock beats Scissor" || playRound(playerSelection, computerSelection) == "You Lose! Scissor beats Paper") {
+            computer++;
+            
+        } 
     }
 
-    if (player > computer) {
-        alert("You Win!");
-    }else if (player < computer) {
-        alert("You Lose!");
-    } else {
-        alert("Draw!");
+
+    if (player >= 5) {
+        result = 'Player Win'
+        reset.style.display = 'block';
+    } else if (computer >= 5) {
+        result = 'Computer Win'
+        reset.style.display = 'block';
     }
+
+    mainResult.innerText = result;
 }
-game();
+
+
+reset.addEventListener('click', () => {
+    mainResult.innerText = '';
+    display.innerText = '';
+    playerSelection = '';
+    result = '';
+    reset.style.display = 'none';
+    player = 0;
+    computer = 0;
+    playerScore.innerText = player;
+    computerScore.innerText = computer;
+    playerChoice.innerText = '';
+    computerChoice.innerText = '';
+     
+
+});
+
+
+
+
+
+        
+    
